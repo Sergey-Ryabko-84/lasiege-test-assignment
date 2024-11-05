32731342af20a39931b1b1fabe7e4b2252e790e2
serhii.ryabko <serhii.ryabko@gmail.com>
2024-11-05 16:50:43 +0200

    adds fixes for deployments

--- frontend/src/api/categoriesApi.js
+++ frontend/src/api/categoriesApi.js
@@ -1,11 +1,8 @@
-const API_BASE_URL = process.env.API_BASE_URL;
-
-const pathToCategories = `${API_BASE_URL}/categories`;
-console.log("API_BASE_URL:", API_BASE_URL);
-console.log("${API_BASE_URL}/categories", pathToCategories);
+// const API_BASE_URL = process.env.API_BASE_URL;
 
 export const fetchCategories = async () => {
-  const response = await fetch(pathToCategories);
+  // const response = await fetch(`${API_BASE_URL}/categories`);
+  const response = await fetch(`/categories`);
   if (!response.ok) {
     throw new Error("Failed to fetch categories");
   }
